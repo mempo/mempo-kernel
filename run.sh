d4844d83b440f55a9a0892875206e4948cdbdf17
Happuri <happuri@gmx.com>
2013-12-03 16:25:51 +0100

    Updated to 3.2.53 kernel version

--- run.sh
+++ run.sh
@@ -1,14 +1,27 @@
 #!/bin/bash 
+# On deterministic-kernel
 
 set -x 
-# download kernel
-mkdir -p kernel-sources/kernel 
+mkdir -p kernel-sources/kernel
 cd kernel-sources/kernel
-wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.2.51.tar.xz  
-# unpack kernel
-unxz linux-3.2.51.tar.xz
-wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.2.51.tar.sig
-# run complilation
-cd ../../kernel-build/linux-3.2.51-securian-0.1.9-shell 
-faketime "2013-10-19 12:58:00" ./all.sh
+wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.2.53.tar.xz
+wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.2.53.tar.sign
+unxz linux-3.2.53.tar.xz
+chmod 755 linux-3.2.53.tar*
+
+#cd ..
+#mkdir -p grsecurity
+#cd grsecurity
+#wget http://grsecurity.net/stable/grsecurity-3.0-3.2.53-201312021727.patch
+#wget http://grsecurity.net/stable/grsecurity-3.0-3.2.53-201312021727.patch.sig
+
+chmod 755 grsecurity-3.0-3.2.53-201312021727.patch*
+
+cd ../..
+
+#deterministic-kernel
+
+cd kernel-build/linux-3.2.53-securian-0.1.10-shell
+faketime "2013-12-02 17:28:00" ./all.sh
 set +x
+
