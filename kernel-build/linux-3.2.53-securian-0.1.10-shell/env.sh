876e09c59f026bd884d48c146ae2b0c167a256cb
mempo-dev2 <opensource@tigusoft.org>
2013-12-09 13:33:50 +0100

    Cleaning up, updating version and timestamp

--- kernel-build/linux-3.2.53-securian-0.1.10-shell/env.sh
+++ kernel-build/linux-3.2.53-securian-0.1.10-shell/env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # deterministic build:
-export TIMESTAMP_RFC3339='2013-08-05 21:51:00-00:00'
+export TIMESTAMP_RFC3339='2013-12-02 17:28:00'
 export KCONFIG_NOTIMESTAMP=1
 export KBUILD_BUILD_TIMESTAMP=`date -u -d "${TIMESTAMP_RFC3339}"`
 export KBUILD_BUILD_USER=user
--- run.sh
+++ run.sh
@@ -3,25 +3,37 @@
 
 set -x 
 mkdir -p kernel-sources/kernel
+
+
+(
+echo "Downloading kernel sources (will verify checksum later - before actually using them)"
 cd kernel-sources/kernel
 wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.2.53.tar.xz
 wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.2.53.tar.sign
 unxz linux-3.2.53.tar.xz
 chmod 755 linux-3.2.53.tar*
-
 #cd ..
+)
+
+
+(
 #mkdir -p grsecurity
 #cd grsecurity
 #wget http://grsecurity.net/stable/grsecurity-3.0-3.2.53-201312021727.patch
 #wget http://grsecurity.net/stable/grsecurity-3.0-3.2.53-201312021727.patch.sig
-
 chmod 755 grsecurity-3.0-3.2.53-201312021727.patch*
+#cd ..
+)
 
-cd ../..
+#cd ..
 
 #deterministic-kernel
 
 cd kernel-build/linux-3.2.53-securian-0.1.10-shell
-faketime "2013-12-02 17:28:00" ./all.sh
+echo 
+echo "Executing the build script"
+echo 
+#faketime "2013-12-02 17:28:00" ./all.sh  # time is set not here, but in env.sh
+./all.sh
 set +x
 
