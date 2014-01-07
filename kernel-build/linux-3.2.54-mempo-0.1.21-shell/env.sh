1b2100bfcbf7a79b0360d76406ef38bafef41d9b
Happuri <happuri@gmx.com>
2014-01-07 14:40:24 +0100

    Changed version Grsecurity kernel patch - removed vserver; updated date (env.sh), sources.list

--- kernel-build/linux-3.2.54-mempo-0.1.21-shell/env.sh
+++ kernel-build/linux-3.2.54-mempo-0.1.21-shell/env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # deterministic build:
-export TIMESTAMP_RFC3339='2013-12-27 01:18:00' # grsecurity-3.0-3.2.53-20131226 20 18.patch +5 h timezone
+export TIMESTAMP_RFC3339='2014-01-05 21:50:00' # grsecurity-3.0-3.2.53-20131226 20 18.patch +5 h timezone
 export KCONFIG_NOTIMESTAMP=1
 export KBUILD_BUILD_TIMESTAMP=`date -u -d "${TIMESTAMP_RFC3339}"`
 export KBUILD_BUILD_USER=user
