5ea91c640556e04e97c07430e1e8f661f1211c03
mempo-dev2 <opensource@tigusoft.org>
2014-02-04 21:15:58 +0100

    [tag] v0.1.23-rc5. build-twice rmdir. Date=mempo

--- build-twice.sh
+++ build-twice.sh
@@ -1,5 +1,5 @@
 
-echo "This script will built the packages, twice, in your HOME folder"
+echo "This script will build the packages, twice, in your HOME folder"
 echo "It will without question REMOVE old data folder"
 echo "This is intended for our alpha-testers and developers mainly"
 echo "Are you sure? type letter y, in upper case. type n to cancel"
@@ -16,26 +16,35 @@ if [[ $y != "Y" ]] ; then
 fi
 
 echo "starting"
+sleep 1
 echo "starting..."
 sleep 2
 
+log1="$HOME/buildlog.log"
+log2="$HOME/builddbg.log"
+
 for nr in 1 2
 do
-	echo "====== BUILD nr=$nr ======"
+	echo "============================================" | tee -a "$log1" "$log2" 
+	echo "=============== BUILD nr=$nr ===============" | tee -a "$log1" "$log2" 
+	echo "============================================" | tee -a "$log1" "$log2" 
+
+	now=$(date) ; echo "TOP LEVEL RUN build nr=$nr at $now" >> "$log1"
 
 	cd ~
 	rm -rf deterministic-kernel/
 	git clone https://github.com/mempo/deterministic-kernel.git
 	cd deterministic-kernel/
 
-	now=$(date) ; echo "RUN build nr=$nr at $now" >> ~/buildlog.txt
+	now=$(date) ; echo "Will executed run.sh at $now" >> "$log1"
 
-	bash run.sh 2>&1 | tee -a ~/builddbg.txt
+	bash run.sh 2>&1 | tee -a "$log2"
 
-	now=$(date) ; echo "DONE build nr=$nr at $now" >> ~/buildlog.txt
+	now=$(date) ; echo "TOP LEVEL DONE build nr=$nr at $now" >> "$log1"
 
 	cd ~
-	mv deterministic-kernel deterministic-kernel-now$nr
+	rm -rf ~/deterministic-kernel-now$nr
+	mv ~/deterministic-kernel ~/deterministic-kernel-now$nr
 
 done
 
--- kernel-build/linux-mempo/env.sh
+++ kernel-build/linux-mempo/env.sh
@@ -5,10 +5,10 @@
 export kernel_general_version="3.2.54" # script uses this setting
 
 # deterministic build:
-export KERNEL_DATE='2014-01-28 23:50:00' # (included +5h timezone)
+export KERNEL_DATE='2014-02-04 20:11:00' # UTC time of mempo version. This is > then max(kernel,grsec,patches) times
 
 # debian make-kpkg related:
-export DEBIAN_REVISION="04" # see README.md how to update it on git tag, on rc and final releases
+export DEBIAN_REVISION="05" # see README.md how to update it on git tag, on rc and final releases
 
 # conversions etc (do not change this)
 export TIMESTAMP_RFC3339=$KERNEL_DATE
