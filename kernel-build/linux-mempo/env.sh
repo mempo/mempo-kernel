01d4f33305aafd5a8755f335099074b9c7125045
vyrly <vyrly@i2pmail.org>
2014-02-13 18:06:08 +0100

    Set build-id=none in all binary files

--- kernel-build/linux-mempo/env.sh
+++ kernel-build/linux-mempo/env.sh
@@ -5,10 +5,10 @@
 export kernel_general_version="3.2.54" # script uses this setting
 
 # deterministic build:
-export KERNEL_DATE='2014-02-07 15:11:00' # UTC time of mempo version. This is > then max(kernel,grsec,patches) times
+export KERNEL_DATE='2014-02-13 17:46:00' # UTC time of mempo version. This is > then max(kernel,grsec,patches) times
 
 # debian make-kpkg related:
-export DEBIAN_REVISION="02" # see README.md how to update it on git tag, on rc and final releases
+export DEBIAN_REVISION="00" # see README.md how to update it on git tag, on rc and final releases
 
 # conversions etc (do not change this)
 export TIMESTAMP_RFC3339=$KERNEL_DATE
