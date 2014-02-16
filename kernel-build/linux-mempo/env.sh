b49e7acd41c7ec9ee0232cca8472325faa489e04
mempo-dev2 <opensource@tigusoft.org>
2014-02-17 00:24:08 +0100

    new linux, grsecurity-3.0-3.2.55-201402152203

--- kernel-build/linux-mempo/env.sh
+++ kernel-build/linux-mempo/env.sh
@@ -2,13 +2,13 @@
 # place for extra settings that could change between releases, and with updates of code and versions
 
 # general:
-export kernel_general_version="3.2.54" # script uses this setting
+export kernel_general_version="3.2.55" # script uses this setting
 
 # deterministic build:
-export KERNEL_DATE='2014-02-13 17:46:00' # UTC time of mempo version. This is > then max(kernel,grsec,patches) times
+export KERNEL_DATE='2014-02-16 23:30:00' # UTC time of mempo version. This is > then max(kernel,grsec,patches) times
 
 # debian make-kpkg related:
-export DEBIAN_REVISION="06" # see README.md how to update it on git tag, on rc and final releases
+export DEBIAN_REVISION="00" # see README.md how to update it on git tag, on rc and final releases
 
 # conversions etc (do not change this)
 export TIMESTAMP_RFC3339=$KERNEL_DATE
