6fc01221200e901039c94a3c840bca781787b0c5
sharedaccount <sharedaccount@nowhere.invalid.com>
2014-02-25 15:17:08 +0100

    [doc] mempo.org is open :-)

--- support.sh
+++ support.sh
@@ -8,7 +8,7 @@ function show_support_info {
 	echo "LOCAL-COPY of part of help pages, doc and FAQ is located here in directory doc-mirror/ !"
 	echo "See #mempo on irc.oftc.net or freenode or irc2p - ask question, wait up to 24 hours, we will reply!"
 	echo "also freenet on FMS boards: linux or mempo (remember to solve captchas) we reply in 1-3 days."
-	echo "also http://mempo.org (planned for 2014-03)"
+	echo "also http://mempo.org"
 	echo "( write down all entire messages, last 20-50 lines of text, this will help to solve your issue )"
 }
 
