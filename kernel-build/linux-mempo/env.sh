bdfae2b8144c4cf03722fccdf14e8b571320d7a5
mempo-dev2 <opensource@tigusoft.org>
2014-03-06 01:06:07 +0100

    [tag]v0.1.30-03-rc changed rev numbering;and fixed

--- kernel-build/linux-mempo/env.sh
+++ kernel-build/linux-mempo/env.sh
@@ -10,7 +10,7 @@ export KERNEL_DATE='2014-03-04 19:37:00' # UTC time of mempo version. This is > 
 export CURRENT_SEED='bd3d86039cc61f25093f4816e05c2d35b4841199776c64a8b6382785f2e55036' # from newest (at release, -6 blocks) http://blockchain.info/block-index/ 
 
 # debian make-kpkg related:
-export DEBIAN_REVISION="02" # see README.md how to update it on git tag, on rc and final releases
+export DEBIAN_REVISION="03" # see README.md how to update it on git tag, on rc and final releases
 
 # conversions etc (do not change this)
 export TIMESTAMP_RFC3339=$KERNEL_DATE
