3e613d13f99db9ae5dee4a38509dfeac29e0c953
vyrly <vyrly@i2pmail.org>
2014-03-07 16:47:10 +0100

    [tag]v0.1.31-01-rc Increase required dpkg version to 0.1.24.6 (fix for #bug1)

--- kernel-build/linux-mempo/env.sh
+++ kernel-build/linux-mempo/env.sh
@@ -5,12 +5,12 @@
 export kernel_general_version="3.2.55" # script uses this setting
 
 # deterministic build:
-export KERNEL_DATE='2014-03-04 19:37:00' # UTC time of mempo version. This is > then max(kernel,grsec,patches) times
+export KERNEL_DATE='2014-03-07 15:41:04' # UTC time of mempo version. This is > then max(kernel,grsec,patches) times
 # Nothing up my sleeve number, unknown in advance even to me in a provable way - e.g. bitcoin markel root of recent block. 
-export CURRENT_SEED='bd3d86039cc61f25093f4816e05c2d35b4841199776c64a8b6382785f2e55036' # from newest (at release, -6 blocks) http://blockchain.info/block-index/ 
+export CURRENT_SEED='8e2676473ce2da500acd008a96d939fed3d988700225b3face8184f4fe7d7c4a' # from newest (at release, -6 blocks) http://blockchain.info/block-index/ 
 
 # debian make-kpkg related:
-export DEBIAN_REVISION="03" # see README.md how to update it on git tag, on rc and final releases
+export DEBIAN_REVISION="01" # see README.md how to update it on git tag, on rc and final releases
 
 # conversions etc (do not change this)
 export TIMESTAMP_RFC3339=$KERNEL_DATE
