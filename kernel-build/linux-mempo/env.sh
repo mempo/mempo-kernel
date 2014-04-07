d531d356d69bcda6e27cb6f8ed8bcf189928b48c
vyrly <vyrly@i2pmail.org>
2014-04-07 09:24:48 +0200

    [tag] v0.1.37-01-rc: linux-3.2.56 kernel and grsecurity update

--- kernel-build/linux-mempo/env.sh
+++ kernel-build/linux-mempo/env.sh
@@ -2,14 +2,14 @@
 # place for extra settings that could change between releases, and with updates of code and versions
 
 # general:
-export kernel_general_version="3.2.55" # script uses this setting
+export kernel_general_version="3.2.56" # script uses this setting
 
 # deterministic build:
-export KERNEL_DATE='2014-04-04 18:51:00' # UTC time of mempo version. This is > then max(kernel,grsec,patches) times
+export KERNEL_DATE='2014-04-07 08:53:00' # UTC time of mempo version. This is > then max(kernel,grsec,patches) times
 # Nothing up my sleeve number, unknown in advance even to me in a provable way - e.g. bitcoin markel root of recent block. 
-export CURRENT_SEED='eccadfa0a04db4673b6f864975cbdd44975604afbda5470456f7394c7291ba7f' # from newest (at release, -6 blocks) http://blockchain.info/block-index/ or http://blockexplorer.com/
+export CURRENT_SEED='fc36a8e1befee61ff5c5d1ca9b90234f2f5d2cd956d73f25c63bd23fbd33ba87' # from newest (at release, -6 blocks) http://blockchain.info/block-index/ or http://blockexplorer.com/
 # debian make-kpkg related:
-export DEBIAN_REVISION="06" # see README.md how to update it on git tag, on rc and final releases
+export DEBIAN_REVISION="01" # see README.md how to update it on git tag, on rc and final releases
 
 # conversions etc (do not change this)
 export TIMESTAMP_RFC3339=$KERNEL_DATE
