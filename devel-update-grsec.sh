e6b910c6df11cbf3a580d0095b962c8f2bcaebad
Happuri <happuri@gmx.com>
2014-04-09 18:56:50 +0200

    Automatically download file, prepare line grsec to sources.list (not tested)

--- devel-update-grsec.sh
+++ devel-update-grsec.sh
@@ -1,3 +1,8 @@
+# using rss to get newest version of gr, rsstail must be installed!
+new_grsec=$(rsstail -u http://grsecurity.net/stable2_rss.php -1 | awk  '{print $2}')
+url="http://grsecurity.net/stable/"$new_grsec
+gr_path='kernel-sources/grsecurity/'
+
 echo ""
 echo "==============================================================="
 echo "This script is for Mempo developers (or users wanting to hack on it)"
@@ -5,6 +10,7 @@ echo "this will take you step by step by process to upgrade Mempo for new grsecu
 echo "in few simple commands :) questions -> #mempo on irc.oftc.net or freenode or see mempo.org"
 echo ""
 echo "This is an UPDATE after grsecurity changed"
+echo "Newest vesrion grsecurity is $new_grsec"
 echo ""
 echo "When ready press ENTER." ; read _
 
@@ -15,6 +21,30 @@ function mywait() {
 function mywait_e() {
 	echo "Press ENTER when ready, I will open editor"
 	read _ 
+} 
+function mywait_d() {
+        echo "Press ENTER to download files"
+        read _ 
+}
+function download() { 
+	echo "Downloading $new_grec from $url " 
+	set -x  
+	cd $gr_path ; rm changelog-stable2.txt ; wget -q  http://grsecurity.net/changelog-stable2.txt  $url $url.sig ; cd ../.. 
+	set +x
+} 
+
+#all line gr in sources.list
+function sources_list() { 
+	new_grsec=$(rsstail -u http://grsecurity.net/stable2_rss.php -1 | awk  '{print $2}')
+	sha256=$(sha256deep  -q  grsecurity-3.0-3.2.56-201404062126.patch)
+	all=P,x,x,grsecurity,$new_grsec,sha256,$sha256,./tmp-path/  
+	all2=$(echo $all | sed -e 's/\ //g')
+
+
+	echo "P,x,x,grsecurity,grsecurity-3.0-3.2.56-201404062126.patch,sha256,a00fefeba886f4f8d7958553ab23fa1df144106ed4c4755e76c1cf56a051a85e,./tmp-path/"
+
+	echo $all2
+
 }
 
 echo "Update sources to github https://github.com/mempo/deterministic-kernel/ or vyrly or rfree (the newest one)" ; mywait 
@@ -22,8 +52,7 @@ echo "Update sources to github https://github.com/mempo/deterministic-kernel/ or
 echo "Read README and info how to update on https://github.com/mempo/deterministic-kernel/ (or local file README.md here)" 
 mywait
 
-echo "[MANUALLY] Download new grsec (to kernel-sources/grsecurity/)" ; mywait
-echo "[MANUALLY] Download new grsec changelog" ; mywait # todo easy automatize
+echo "[AUTO] Download new grsec (to kernel-sources/grsecurity/)" ; mywait_d ; download
 
 echo ""
 ls -rtl "kernel-sources/grsecurity/"
