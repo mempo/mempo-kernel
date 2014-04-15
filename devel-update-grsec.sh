b7d5939025927cf622bc21710761ef8a826b131f
Happuri <happuri@gmx.com>
2014-04-15 15:10:19 +0200

    Auto update sources.list file (grsec)

--- devel-update-grsec.sh
+++ devel-update-grsec.sh
@@ -33,22 +33,38 @@ function mywait_d() {
 function download() { 
 	echo "Downloading $new_grec from $url " 
 	set -x  
-	cd $gr_path ; rm changelog-stable2.txt ; wget -q  http://grsecurity.net/changelog-stable2.txt  $url $url.sig ; cd ../.. 
+	cd $gr_path ; rm changelog-stable2.txt ; wget -q  http://grsecurity.net/changelog-stable2.txt  $url $url.sig 
+	sha256=$(sha256deep  -q  $new_grsec)  ;  cd ../..
+
 	set +x
 } 
 
 #all line gr in sources.list
 function sources_list() { 
-	# TODO finish and use this
-
-	echo "Debug in sources_list, new_grsec=$new_grsec" 1>&2
-	exit ;
-	# new_grsec=$(rsstail -u http://grsecurity.net/stable2_rss.php -1 | awk  '{print $2}')
-	sha256=$(sha256deep  -q  grsecurity-3.0-3.2.56-201404062126.patch)  # TODO FIX XXX other file
+	echo "Updating sources.list"
+	cd kernel-build/linux-mempo/
+	# echo "Debug in sources_list, new_grsec=$new_grsec" 1>&2
+	# exit ;
 	all=P,x,x,grsecurity,$new_grsec,sha256,$sha256,./tmp-path/  
 	all2=$(echo $all | sed -e 's/\ //g')
-	echo "P,x,x,grsecurity,grsecurity-3.0-3.2.56-201404062126.patch,sha256,a00fefeba886f4f8d7958553ab23fa1df144106ed4c4755e76c1cf56a051a85e,./tmp-path/"
 	echo $all2
+	file='sources.list'
+	tmp='sources.list.tmp'
+	mv $file $tmp
+	let i=0
+	
+	for line in $(cat $tmp); do
+        	let i=$i+1
+		if [[ $i -eq 2 ]]       
+		then    
+			echo $all2 >> $file  
+			echo "Line: $all2 was saved" 
+        	else  
+                	echo $line >> $file 
+        	fi 
+	done 
+	rm $tmp
+	cd ../..
 }
 
 echo "Update sources to github https://github.com/mempo/deterministic-kernel/ or vyrly or rfree (the newest one)" ; mywait 
@@ -56,22 +72,21 @@ echo "Update sources to github https://github.com/mempo/deterministic-kernel/ or
 echo "Read README and info how to update on https://github.com/mempo/deterministic-kernel/ (or local file README.md here)" 
 mywait
 
-echo "[AUTO] Download new grsec (to kernel-sources/grsecurity/)" ; mywait_d ; download
+echo "[AUTO] Downloadng new grsec (to kernel-sources/grsecurity/) and updating sources.list" ; mywait_d ; download
+sources_list
+# echo ""
+# ls -rtl "kernel-sources/grsecurity/"
+# echo "What is the file name of new grsec .patch? (copy/type the filename and press enter)"; echo -n "> " ; read gr_patch_file
+# gr_patch="kernel-sources/grsecurity/$gr_patch_file"
+# echo ""
 
-echo ""
-ls -rtl "kernel-sources/grsecurity/"
-echo "What is the file name of new grsec .patch? (copy/type the filename and press enter)"; echo -n "> " ; read gr_patch_file
-gr_patch="kernel-sources/grsecurity/$gr_patch_file"
-echo ""
+# echo "Add new textblock for new mempo version (increase with new grsec), you said grsec is $gr_patch_file" ; mywait_e
+# vim changelog
 
-echo "Add new textblock for new mempo version (increase with new grsec), you said grsec is $gr_patch_file" ; mywait_e
-vim changelog
-
-sha256sum $gr_patch || { echo "Can not checksum $gr_patch" ; exit 1; }
-echo "Copy the above CHECKSUM and FILENAME to replace both in line with grsecurity in the file..."
-mywait_e
-vim kernel-build/linux-mempo/sources.list
-
+# sha256sum $gr_patch || { echo "Can not checksum $gr_patch" ; exit 1; }
+# echo "Copy the above CHECKSUM and FILENAME to replace both in line with grsecurity in the file..."
+# mywait_e
+# vim kernel-build/linux-mempo/sources.list
 echo "Now we will increase mempo version"
 mywait
 . devel-update-version.sh
