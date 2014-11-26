5b3949cc42d8dba370cad4e86fa4a5cbe6a624bc
mempomisc <mempo.misc@onet.pl>
2014-11-26 13:47:46 +0100

    Test script updates

--- run-test.sh
+++ run-test.sh
@@ -1,11 +1,29 @@
 #!/bin/bash -e
+FLAVOUr="$1"
+version=$( git describe --tags )
+date=$( date -u +%s )
+computer=$HOSTNAME
+
+date_start=$( date -u +%s )
 
 ./run-flavour "$1" "$2" "$3" || { echo "Build failed, aborting" ; exit 1; }
 
-flavour="$1"
-version=$( git describe --tags )
-dir="$HOME/test/$version/$flavour/$date"
-mkdir -p "$dir" 
+date_done=$( date -u +%s )
 
-cp -var "kernel-build/linux-mempo/*.deb"  "$dir/"
+seconds=$(( date_done - date_start ))
+
+pwd=$PWD
+cd kernel-build/linux-mempo
+sums_eol="$(sha1sum *.deb )"
+sums="$(echo $sums_eol)" # flatten it into one line with no /n
+cd $pwd
+
+echo 
+echo "Build $flavour ver $version on $computer in $seconds sec, Sums: $sums" | tee -a ~/result.txt
+echo 
+
+dir="$HOME/test/$version/$flavour/$date"
+echo "Dir is: $dir"
+mkdir -p "$dir" 
+cp -var kernel-build/linux-mempo/*.deb  "$dir/"
 
