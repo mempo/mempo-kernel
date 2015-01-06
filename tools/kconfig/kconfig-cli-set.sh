23f638347e5a436e9abb35fb728e26cd0900f84d
rfree <rfree@gmx.us>
2015-01-06 17:18:20 +0100

    kconfig script supports multi word strings

--- tools/kconfig/kconfig-cli-set.sh
+++ tools/kconfig/kconfig-cli-set.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+# Kernel config commandline/batch editor; 
+# (C) 2014 mempo.org team ; BSD Licence
+# https://github.com/mempo/deterministic-kernel/blob/master/tools/kconfig/kconfig-cli-set.sh
+# 
+# Usage:
+#
+# kconfig-cli-set.sh config_file [CONFIG_NAME=CONFIG_VALUE] ... 
+# kconfig-cli-set.sh .config OPT_N=- OPT_Y=y OPT_S=shortstring 'OPT_L="new long string"' OPT_V1024=1024 
+#
+# 1st argument is filename of the .config file to edit
+# Each next argument is in form N=V where N is name of CONFIG_ option, and V is the value.
+#   Value '-' wors as "n", it means to delete the config and instead write comment: # $N is not set
+#   Value that is a word like y,m, (and even n) or FOO is just used as single-word value
+#   To use multi word string you must quote the value and quote the entire command like in example above
+# See also test.sh file for example
 
 normal_exit=n
 function my_die() {
@@ -36,9 +51,9 @@ function change_one_setting() {
 	if [[ "$V" == 'n' ]] ; then echo "Warning: value 'n' is used, if you want to configure option to No then set it to value '-' (minus sign). In option ($N)" ; fi
 	if [[ "$V" == 'N' ]] ; then echo "Warning: value 'N' is used, if you want to configure option to No then set it to value '-' (minus sign). In option ($N)" ; fi
 
-	if [[ $V =~ .*\".* ]] ; then my_die "Do NOT use quotation markers in the value (sorry we do not support multi-word edits now yet, but it's easy to hack in just use function change_one_setting" ; fi
+#	if [[ $V =~ .*\".* ]] ; then my_die "Do NOT use quotation markers in the value (sorry we do not support multi-word edits now yet, but it's easy to hack in just use function change_one_setting" ; fi
 
-	if [[ $V =~ .*\'.*\'* ]] ; then my_die "Do NOT use quotation markers in the value (sorry we do not support multi-word edits now yet, but it's easy to hack in just use function change_one_setting" ; fi
+#	if [[ $V =~ .*\'.*\'* ]] ; then my_die "Do NOT use quotation markers in the value (sorry we do not support multi-word edits now yet, but it's easy to hack in just use function change_one_setting" ; fi
 	# this above regexp is just: .*\'.* meaning .*'.* so any string that contains single-quote (') character, the additional string '* at end is just to fix some source code editors that get confused here (e.g. vim in debian 7)
 	
 	if [[ $V == '-' ]] ;
@@ -63,7 +78,7 @@ function change_one_setting() {
 
 cp "$argument_input" "$tempdir/start"
 
-for pair in $*
+for pair in "$@"
 do
 	# echo "REPLACE: ${pair}"
   # split line at "=" sign
@@ -71,10 +86,11 @@ do
 	IFS="="
 	read -r VAR VAL <<< "${pair}"
 	IFS="${IFS_OLD}"
+	# echo "pair=$pair" ; echo "VAR=$VAR" ; echo "VAL=$VAL" # debug
 	change_one_setting "$VAR" "$VAL"
 done
 
-diff "$tempdir/start" "$argument_input"
+# diff "$tempdir/start" "$argument_input" # debug
 
 normal_exit='y'
 
--- tools/kconfig/test.sh
+++ tools/kconfig/test.sh
@@ -1,7 +1,7 @@
 
-cp .config.example  .config
+cp config.example  config
 
-bash kconfig-cli-set.sh .config OPT_Y=- OPT_N=y OPT_S=newname OPT_V=1024 OPT_M=y
+bash kconfig-cli-set.sh config  OPT_S=new OPT_Y1=y OPT_Y2=y OPT_M=m OPT_N=- 'OPT_L="new long string"' OPT_V1024=1024
 
 # echo "Results:"
 # diff .config.example  .config
@@ -9,6 +9,8 @@ bash kconfig-cli-set.sh .config OPT_Y=- OPT_N=y OPT_S=newname OPT_V=1024 OPT_M=y
 echo 
 echo 
 echo "Is the file as expected after edit:"
-diff .config .config.example2
+set -x
+diff config config.example.out
+set +x
 echo 
 
