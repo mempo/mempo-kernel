d9af7d87cc00f0ea5519ba000a5394752377cf15
rfree-mempo <rfree@i2pmail.org>
2015-02-21 01:09:04 +0100

    git get/push script update

--- git-master-push.sh
+++ git-master-push.sh
@@ -5,16 +5,34 @@ echo "pause... enter to continue" ; read _
 
 git merge --ff-only mempo/master 
 git merge --ff-only rfree/master 
-echo "--- log ---"
-git log | head
+git merge --ff-only mempomisc/master 
+git merge --ff-only 0x20c24/master
+
+echo "--- log (files) ---"
+
+git log --show-signature  --name-only HEAD^^^..HEAD
+
+echo "--- log (info) ---"
+git log --show-signature  HEAD^^..HEAD | head -n 50
+
 echo "pause... enter to continue: CHCK IF LOG IS OK (is that latest commit?)" ; read _
 
+echo "Choose tag"
 git tag
 
 echo "What tag should we sign with?"
 read tag
 
-git tag -s $tag 
+echo "---------------------------------------------------------------"
+git log --show-signature  HEAD^^..HEAD | head -n 50
+echo "---------------------------------------------------------------"
+head -n 10 changelog
+echo "---------------------------------------------------------------"
+cat cannary.txt
+echo "" ; echo "^--- copy paste the above (and press ENTER)" ; read _
+
+git tag -s $tag
+
 git tag
 
 echo "pause... enter to continue" ; read _
