c2a4c10b4a1cb89f081608c62a31d8c3fb27a5c1
rfree-mempo <rfree@i2pmail.org>
2015-05-05 10:02:05 +0200

    git script: ask when can not merge

--- git-master-push.sh
+++ git-master-push.sh
@@ -1,12 +1,35 @@
 #!/bin/bash -e
 
-git fetch --all --tags 
-echo "pause... enter to continue" ; read _
+git fetch --all --tags || {
+	echo "Can not download remote changes - abortig"
+	exit 1
+}
 
-git merge --ff-only mempo/master 
-git merge --ff-only rfree/master 
-git merge --ff-only mempomisc/master 
-git merge --ff-only 0x20c24/master
+#echo "pause... enter to continue" ; read _
+
+count_err=0
+for repo in mempo/master rfree/master mempomisc/master 0x20c24/master
+do
+	git merge --ff-only "$repo" || { count_err=$(( count_err+1 )) ; echo "Can not merge $repo" ; }
+done
+
+echo "OK done (errors=$count_err) merging remote changes"
+
+if [ "$count_err" -gt "0" ]
+then
+	echo "**************************"
+	echo "CAN NOT MERGE SOME CHANGES"
+	echo "**************************"
+	echo "Continue? (for example because you edited last commit message) y/n"
+	read yn
+	if [[ "$yn" != "y" ]] 
+	then 
+		exit 1
+	fi
+else
+	echo "Remote changes merged without problems"
+fi
+
 
 echo "--- log (files) ---"
 
